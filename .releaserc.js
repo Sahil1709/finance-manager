2462f354eebdbd9478d0842631ec35d11b75e925
sahil <sahilofficial17@gmail.com>
2025-03-15 23:19:19 -0700

    chore: migrate semantic release configuration from JSON to JS format

---
+++ .releaserc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  branch: "main",
+  repositoryUrl: "https://github.com/Sahil1709/finance-manager.git",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [],
+        githubToken: process.env.GH_PAT_TOKEN
+      }
+    ]
+  ]
+};
