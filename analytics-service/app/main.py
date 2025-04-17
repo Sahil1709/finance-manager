9d0741355cfc25f6a92454f7fe2c5ad777d83e2b
sahil <sahilofficial17@gmail.com>
2025-04-17 00:53:11 -0700

    Added a new microservice analytics service with Docker support, add FastAPI setup, and define models for transactions and budgets

---
+++ analytics-service/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .analytics import router as analytics_router
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app = FastAPI(title="Analytics Service")
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=[os.getenv("FRONTEND_URL")],
+  allow_methods=["GET"],
+  allow_headers=["*"],
+)
+app.include_router(analytics_router)
